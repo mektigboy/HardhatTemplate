3362e11c30d8bf7aa093e420a91aab3ff6746b99
mektigboy <mektigboy@protonmail.com>
2022-08-16 19:43:09 -0500

    Update utils/verify.js

---
+++ utils/verify.js
@@ -0,0 +1,22 @@
+const { network, run } = require("hardhat");
+const { networkConfig } = require("../helper-hardhat-config");
+
+const verify = async (contractAddress, arguments) => {
+    console.log("Verifying contract...");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: arguments,
+        });
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!");
+        } else {
+            console.log(error);
+        }
+    }
+};
+
+module.exports = {
+    verify,
+};
